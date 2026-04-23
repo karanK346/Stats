a0cd7e0e69274a81cbcbf8b9615ccf89de10e94a
karanK346 <karan.kullar@ucalgary.ca>
2026-04-23 14:57:27 -0600

    Delete app directory

--- app/config.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / "data"
-DATA_DIR.mkdir(exist_ok=True)
-DB_PATH = DATA_DIR / "sports_cache.sqlite3"
